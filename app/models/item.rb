9317732aef76fa4e0e3ee49d235a508c7388b6ab
yuri-lily <yuri.lilyy.xx@gmail.com>
2023-07-09 17:40:11 +0900

    商品一覧表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one :order
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -11,7 +12,8 @@ class Item < ApplicationRecord
 
   validates :image, :name, :info, presence: true
 
-  validates :price, presence: true,numericality: { only_intenger: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :price, presence: true,
+                    numericality: { only_intenger: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
