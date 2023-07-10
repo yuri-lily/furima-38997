70962baaa9c43af35c499963918a4e467cfec068
yuri-lily <yuri.lilyy.xx@gmail.com>
2023-07-11 00:40:00 +0900

    修正2
    authenticate_user!にeditを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_params, only: [:show, :edit, :update]
   before_action :move_to_index, except: [:index, :show]
 
