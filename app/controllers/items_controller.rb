6dcc00b4eba806ffb6d334c797ce9f8d08d3fc91
yuri-lily <yuri.lilyy.xx@gmail.com>
2023-07-11 15:36:06 +0900

    修正3
    move_to_index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
-    return if current_user == @item.user
-
-    redirect_to root_path
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 end
