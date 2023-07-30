cffa1be65a13850a0dec68e466e9868fbbfe3bc0
yuri-lily <yuri.lilyy.xx@gmail.com>
2023-07-30 11:53:22 +0900

    プレビュー機能の実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('new_post');
+  const previewList = document.getElementById('previews');
+  if (!postForm) return null;
+  console.log("preview.jsが読み込まれました");
+
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]')
+  fileField.addEventListener('change', function(e){
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
+});
